0d2c6af0d32f27080b5b14a1b67da42a1645e8ba
Joy Lin <vvlnote@hotmail.com>
2019-06-14 23:51:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(streetNo) {
+  const hqStreetNo = 42;
+  let blocks = 0;
+  if (hqStreetNo < streetNo ) {
+    blocks = streetNo - hqStreetNo;
+  } else {
+    blocks = hqStreetNo - streetNo;
+  }
+  return blocks;
+}
+
+function distanceFromHqInFeet(streetNo){
+  const blockDistance = 264;
+  let blocks = distanceFromHqInBlocks(streetNo);
+  return blocks*blockDistance;
+}
